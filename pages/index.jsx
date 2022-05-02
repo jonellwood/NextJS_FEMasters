1b382dda3a22b31f717eecdd81ad8c6f20dd8ab8
Jon Ellwood <jonellwood@gmail.com>
2022-05-02 14:37:02 -0400

    up to deployments

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,27 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import Link from 'next/link'
+import { jsx } from 'theme-ui';
+import Link from 'next/link';
+import Browser from '../src/components/browser';
+import Dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 
-export default () => (
+const BrowserComponent = dynamic(() => import('../src/components/browser'), {ssr: false, loading: () => <p>Loading bro...</p>})
+
+export default ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
-      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+      <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
+    <BrowserComponent />
     </div>
   </div> 
 )
+
+export function getStaticProps() {
+  // get fake data from a fake CMS
+  
+  return {
+    props: {content: {
+      title: "This is my nice notes app"
+    }}
+  }
+}
