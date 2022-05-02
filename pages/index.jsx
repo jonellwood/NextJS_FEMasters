dc0f986ab892cab2c1be4dd03fab440a65a57412
Jon Ellwood <jonellwood@gmail.com>
2022-05-02 15:10:47 -0400

    removed browser component

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
       <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
-    <BrowserComponent />
+    {/* <BrowserComponent /> */}
     </div>
   </div> 
 )
